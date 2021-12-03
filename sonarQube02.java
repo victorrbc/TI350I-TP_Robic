3e9a6dec61766f5ebc4762965e2e06cff6d2819e
victorrbc <75671640+victorrbc@users.noreply.github.com>
2021-12-03 16:50:09 +0100

    Update sonarQube02.java

--- sonarQube02.java
+++ sonarQube02.java
@@ -1,4 +1,4 @@
-public class sonarQube02 {
+public class SonarQube02 {
 
     String printNumbers(String stringToAdd, int x, int y) {
         return String.format("Numbers are %s %d %d", stringToAdd, x, y);
